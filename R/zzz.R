db38ae6b8a0edf3ecca0bad5c9522a389d75f397
afsarchowdhury <a.chowdhury@hydehighschool.uk>
2021-08-26 10:21:24 +0100

    Update startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,7 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(paste0(
-    "Welcome to g4sr.",
-    "\n\nThis package is released under GPLv3.",
-    "\n\nLog issues at https://github.com/afsarchowdhury/g4sr/issues.",
-    "\n\nYou will need a valid API key to access the functions in this pacakge.",
-    "\nAsk your school administrator to generate one for you."
+    "You will need a valid API key to access the functions in this pacakge.",
+    "\nAsk your school administrator to generate one for you.",
+    "\nLog errors and bugs at https://github.com/afsarchowdhury/g4sr/issues."
   ))
 }
