0917e55449211bf9433cbefbaf3cd36a76293146
afsarchowdhury <a.chowdhury@hydehighschool.uk>
2023-06-02 12:51:18 +0100

    Update startup message for citation.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,13 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(paste0(
-    "You will need a valid API key to access the functions in this pacakge.",
-    "\nAsk your school administrator to generate one for you.",
-    "\nLog errors and bugs at https://github.com/afsarchowdhury/g4sr/issues."
-  ))
+  packageStartupMessage(
+    paste0(
+      "You will need a valid API key to access the functions in this pacakge.",
+      "\nAsk your school administrator to generate one for you.",
+      "\nLog errors and bugs at https://github.com/afsarchowdhury/g4sr/issues."
+    )
+  )
+
+  packageStartupMessage(
+    message(cat(crayon::cyan("Type citation('g4sr') to cite g4sr in a publication.")))
+  )
 }
